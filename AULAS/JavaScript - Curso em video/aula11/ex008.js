84c62f187c6ce07b33d7454a0ec7401817070999
Patrick <patrick.brining@outlook.com>
2022-07-03 22:18:39 -0300

    aulas JS CeV

---
+++ AULAS/JavaScript - Curso em video/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 100
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { // Condição simples
+    console.log('Você ultapassou a velodidade permitida. MULTADO!')
+}
+
+console.log('Diriga sempre com segurança!')
+
---
+++ AULAS/JavaScript - Curso em video/aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'eua'
+if (pais.toUpperCase() == 'BRASIL') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
