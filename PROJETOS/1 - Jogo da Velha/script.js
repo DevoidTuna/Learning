1bbb67edc042e245fe273a1d3cb190d709ff19dc
Patrick <patrick.brining@outlook.com>
2022-07-27 20:26:58 -0300

    Update tic-tac-toe não finalizado

--- PROJETOS/1 - Jogo da Velha/script.js
+++ PROJETOS/1 - Jogo da Velha/script.js
@@ -1,62 +1,49 @@
-var status = document.querySelector("#status");
-var player1PontosTXT = document.querySelector("#player1");
-var player2PontosTXT = document.querySelector("#player2");
-var player1 = document.querySelector("#nome-player1").value
-var player2 = document.querySelector("#nome-player2").value
-var divJogo = document.querySelector("#jogo")
-var divMenu = document.querySelector("#menu")
-var modosJogo = document.querySelector("#modos-jogo")
-var preMultiplayer = document.querySelector("#nome-jogadores")
-var b1 = document.querySelector("#b1");
-var b2 = document.querySelector("#b2");
-var b3 = document.querySelector("#b3");
-var b4 = document.querySelector("#b4");
-var b5 = document.querySelector("#b5");
-var b6 = document.querySelector("#b6");
-var b7 = document.querySelector("#b7");
-var b8 = document.querySelector("#b8");
-var b9 = document.querySelector("#b9");
-var player1Pontos = 5
-var player2Pontos = 5
+var telaMenu = document.querySelector("#menu")
+var telaJogo = document.querySelector("#jogo")
+var statusTXT = document.querySelector("#status")
+var bloco = document.getElementsByClassName("bloco")
+var jogador = "X"
+var quemComecou = 'X'
+var vez = document.querySelector("#vez")
+var posicoes = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+]
+const posicoesVitoria = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+var ganhador = '_'
 
-
-function jogar() {
-    player1PontosTXT.innerHTML = `${player1}: ${player1Pontos} PTS`
-    player2PontosTXT.innerHTML = `${player2}: ${player2Pontos} PTS`
-    divMenu.style.display = "none"
-    divJogo.style.display = "block"
+function alternador() {
+    if(jogador == 'X') {
+        return jogador = 'O'
+    } else {
+        return jogador = 'X'
+    }
 }
 
-function zerar() {
-    player1Pontos = 0
-    player2Pontos = 0
-    jogar()
+for(let a = 0; a < 9; a++) {
+    function clicar(i) {
+        if(bloco[i].value == '' && ganhador == '_') {
+            if(jogador == 'X') {
+                posicoes[i] = i = 1
+            } else {
+                posicoes[i] = i = -1
+            }
+            bloco[i].value = jogador
+            alternador()
+            vez.innerHTML = jogador
+            statusTXT.innerHTML = posicoes
+            checarVencedor()
+        }
+    }
 }
 
-function menu() {
-    divMenu.style.display = "block"
-    divJogo.style.display = "none"
-    modosJogo.style.display = "block"
-    preMultiplayer.style.display = "none"
-}
-
-function reiniciar() {
-    jogar()
-}
-
-function modoMultiplayer() {
-    modosJogo.style.display = "none"
-    preMultiplayer.style.display = "block"
-}
-
-function modoSingleplayerFacil() {
-
-}
-
-function modoSingleplayerDificil() {
-
-}
-
-function colocar() {
-
-}
