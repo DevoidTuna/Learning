f8f31f8c3b1faa3c0c0ce352af531f7d65ec5ff3
Patrick <patrick.brining@outlook.com>
2022-08-06 01:31:49 -0300

    Update forma inteligente

--- PROJETOS/1 - Jogo da Velha/script.js
+++ PROJETOS/1 - Jogo da Velha/script.js
@@ -1,27 +1,71 @@
-var telaMenu = document.querySelector("#menu")
-var telaJogo = document.querySelector("#jogo")
-var statusTXT = document.querySelector("#status")
-var bloco = document.getElementsByClassName("bloco")
-var pontosXTXT = document.querySelector("#pontosX")
-var pontosOTXT = document.querySelector("#pontosO")
-var cor = document.getElementsByTagName('b')
-var btnReiniciar = document.querySelector('#btn-reiniciar-rodape')
-var jogador = 'X'
-var quemComecou = 'O'
-var pontosX = 0
-var pontosO = 0
+const telaMenu = document.querySelector("#menu")
+const telaJogo = document.querySelector("#jogo")
+const statusTXT = document.querySelector("#status")
+const bloco = document.getElementsByClassName("bloco")
+const pontosXTXT = document.querySelector("#pontosX")
+const pontosOTXT = document.querySelector("#pontosO")
+const btnReiniciar = document.querySelector('#btn-reiniciar-rodape')
+let jogador = 'X'
+let quemComecou = 'O'
+let pontosX = 0
+let pontosO = 0
+
+let posicoes = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+]
+
+const posicoesVitoria = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 
 reiniciar()
 
 function clicar(i) {
     if(bloco[i].value == ' ') {
         bloco[i].value = jogador
+        if(jogador == 'X') {
+            posicoes[i] += 1
+        } else if(jogador == 'O') {
+            posicoes[i] -= 1
+        }
         alternador()
         statusTXT.innerHTML = `${jogador}`
     }
     checagem()
     pontosXTXT.innerHTML = `Pontos X: ${pontosX}`
     pontosOTXT.innerHTML = `Pontos O: ${pontosO}`
+}
+
+function checagem() {
+    for(let i = 0; i < 8; i++) {
+        let check = posicoes[posicoesVitoria[i][0]] + posicoes[posicoesVitoria[i][1]] + posicoes[posicoesVitoria[i][2]]
+        let empate = (posicoes.every( (e) => e != 0))
+        if(check == 3) {
+            statusTXT.innerHTML = 'Vitória X'
+            destaque(bloco[posicoesVitoria[i][0]], bloco[posicoesVitoria[i][1]], bloco[posicoesVitoria[i][2]])
+        } else if(check == -3) {
+            statusTXT.innerHTML = 'Vitória O'
+            destaque(bloco[posicoesVitoria[i][0]], bloco[posicoesVitoria[i][1]], bloco[posicoesVitoria[i][2]])
+        } else if(empate == true) {
+            statusTXT.innerHTML = 'EMPATE'
+            statusTXT.style.color = 'yellow'
+            for(let i = 0; i < 9; i++) {
+                bloco[i].style.color = 'yellow'
+                btnReiniciar.style.backgroundColor = '#ffff00'
+                btnReiniciar.style.color = 'black'
+            }
+        }
+    }
 }
 
 function alternador() {
@@ -42,6 +86,76 @@ function quemComeca() {
     }
 }
 
+
+
+function destaque(x, y, z) {
+    for(let i = 0; i < 9; i++) {
+        bloco[i].disabled = true
+        bloco[i].style.color = 'gray'
+    }
+    if(x.value == 'X') {
+        x.style.color = 'red'
+        y.style.color = 'red'
+        z.style.color = 'red'
+        statusTXT.style.color = 'red'
+        btnReiniciar.style.backgroundColor = '#ff0000'
+        btnReiniciar.style.color = 'white'
+    } else if(x.value == 'O') {
+        x.style.color = 'blue'
+        y.style.color = 'blue'
+        z.style.color = 'blue'
+        statusTXT.style.color = 'blue'
+        btnReiniciar.style.backgroundColor = '#0000ff'
+        btnReiniciar.style.color = 'white'
+    }
+}
+
+function reiniciar() {
+    for(let i = 0; i <9; i++) {
+        bloco[i].disabled = false
+        bloco[i].value = ' '
+        bloco[i].style.color = 'white'
+        statusTXT.style.color = 'white'
+        btnReiniciar.style.color = 'black'
+        btnReiniciar.style.backgroundColor = '#ffffff'
+        posicoes[i] = 0
+    }
+    quemComeca()
+    statusTXT.innerHTML = `${jogador} começa`
+}
+
+/*
+function checar() {
+    let roundWon = false
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = posicoesVitoria[i]
+        let a = posicoes[winCondition[0]]
+        let b = posicoes[winCondition[1]]
+        let c = posicoes[winCondition[2]]
+        if (a === 0 || b === 0 || c === 0) {
+            continue
+        }
+        if (a === b && b === c) {
+            roundWon = true
+            break
+        }
+    }
+    if (roundWon) {
+            statusTXT.innerHTML = `O jogador ${jogador}`
+            return
+    }
+}
+
+function checar2() {
+    for(let i = 0; i <=7; i++) {
+        let posVitoria = posicoesVitoria[i]
+        if((posicoes.includes(posicoesVitoria[i])) == posVitoria) {
+            statusTXT.innerHTML = 'ACERTOOOUUUU'
+        }
+    }
+}
+*/
+
 /*
 Para toda posição de vitória (x1, x2, x3) faça:
     soma = posicoes[x1] + posicoes[x2] + posicoes[x3]     
@@ -50,7 +164,7 @@ Para toda posição de vitória (x1, x2, x3) faça:
 */
 
 // Fiz o modo mais braçal no momento, conforme vou adquirindo conhecimento e experiencia vou fazer este código de uma forma mais inteligente
-function checagem() {
+/* function checagem() {
     if(bloco[0].value == bloco[1].value && bloco[1].value == bloco[2].value) {
         if(bloco[0].value === 'O') {
             pontosO += 1
@@ -150,71 +264,4 @@ function checagem() {
             btnReiniciar.style.color = 'black'
         }
     }
-}
-
-function destaque(x, y, z) {
-    for(let i = 0; i < 9; i++) {
-        bloco[i].disabled = true
-        bloco[i].style.color = 'gray'
-    }
-    if(x.value == 'X') {
-        x.style.color = 'red'
-        y.style.color = 'red'
-        z.style.color = 'red'
-        statusTXT.style.color = 'red'
-        btnReiniciar.style.backgroundColor = '#ff0000'
-        btnReiniciar.style.color = 'white'
-    } else if(x.value == 'O') {
-        x.style.color = 'blue'
-        y.style.color = 'blue'
-        z.style.color = 'blue'
-        statusTXT.style.color = 'blue'
-        btnReiniciar.style.backgroundColor = '#0000ff'
-        btnReiniciar.style.color = 'white'
-    }
-}
-
-function reiniciar() {
-    for(let i = 0; i <9; i++) {
-        bloco[i].disabled = false
-        bloco[i].value = ' '
-        bloco[i].style.color = 'white'
-        statusTXT.style.color = 'white'
-        btnReiniciar.style.color = 'black'
-        btnReiniciar.style.backgroundColor = '#ffffff'
-    }
-    quemComeca()
-    statusTXT.innerHTML = `${jogador} começa`
-}
-
-/*
-function checar() {
-    let roundWon = false
-    for (let i = 0; i <= 7; i++) {
-        const winCondition = posicoesVitoria[i]
-        let a = posicoes[winCondition[0]]
-        let b = posicoes[winCondition[1]]
-        let c = posicoes[winCondition[2]]
-        if (a === 0 || b === 0 || c === 0) {
-            continue
-        }
-        if (a === b && b === c) {
-            roundWon = true
-            break
-        }
-    }
-    if (roundWon) {
-            statusTXT.innerHTML = `O jogador ${jogador}`
-            return
-    }
-}
-
-function checar2() {
-    for(let i = 0; i <=7; i++) {
-        let posVitoria = posicoesVitoria[i]
-        if((posicoes.includes(posicoesVitoria[i])) == posVitoria) {
-            statusTXT.innerHTML = 'ACERTOOOUUUU'
-        }
-    }
-}
-*/
+} */
