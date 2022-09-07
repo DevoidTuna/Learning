e53a8225186024e4b2c326137e6b30196ed5f55b
Patrick <patrick.brining@outlook.com>
2022-09-06 21:12:55 -0300

    atualizações pequenas

--- AULAS/PHP - He4rt/ambiente.php
+++ AULAS/PHP - He4rt/ambiente.php
@@ -1,5 +1,57 @@
 <?php
 
-echo 'Patrick Brining'
+// echo 'Patrick Brining' . PHP_EOL;
 
-?>
+/*
+comentario
+de varias
+linhas - vulgo DockBlock
+*/
+
+// var_dump('Patrick');
+// var_dump(true); //Booleans NÃO são CaseSensitive
+// var_dump(1234.12312);
+
+// $arr = ['Patrick', 19, true];
+
+// var_dump($arr);
+
+// $patrick = [
+//     'nome' => 'Patrick',
+//     'idade' => 19,
+//     'cidade' => 'itajai'
+// ];
+
+// echo $patrick['idade'];
+
+// define('idadeDoPatrick', 19);
+
+// echo idadeDoPatrick;
+
+// $um = [
+//     'nome' => 'Patrick',
+//     'idade' => 19
+// ];
+
+// $dois = [
+//     'profissao' => 'Programador',
+//     'empresa' => 'Dataclick'
+// ];
+
+// $concatenado = $um + $dois;
+
+// print_r($concatenado);
+
+// print_r('abcdef' . PHP_EOL);
+
+
+
+var_dump($argv);
+$comando = `code .`;
+
+if ($argv[1] == 'Patrick') {
+    echo 'Olá Patrick, eu sou o PHP e abri o VSCode.' . PHP_EOL;
+    echo $comando;
+}
+
+
