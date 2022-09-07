60a0f2ce412a305ec178e6e608579f46690086e5
Patrick <patrick.brining@outlook.com>
2022-09-06 21:28:14 -0300

    banco 1.2 sem testar

--- PROJETOS/2 - Banco (OOP FIX)/app.js
+++ PROJETOS/2 - Banco (OOP FIX)/app.js
@@ -1,15 +1,13 @@
 const Apresentação = `Bem vindo ao script básico de banco. Caso precise de ajuda, digite: ajuda()\n`
 
 const ajuda = () =>
-    console.log(
-        `
+        console.log(`
         Acessar o Usuário -> listaUsuarios[n°ID] {alterar n°ID pelo número do ID}.
         -----------------
         criarUsuario(CPF, 'nome') -> Retornará o ID do Usuário caso seu CPF já esteja cadastrado,
         ou criará um novo Usuário.
         -----------------
-        buscarUsuario(CPF ou ID) -> Buscará o Usuário com o CPF ou o ID cadastrado.
-        buscarUsuario('nome') -> Buscará *todos* os Usuários com o mesmo nome.
+        cpfOuIDrUsuario(CPF ou ID) -> cpfOuIDrá o Usuário com o CPF ou o ID cadastrado.
         -----------------
         Usuario.criarContaCorrente() -> Caso o Usuário já tenha Conta Corrente, retornará o ID da sua Conta,
         caso o Usuário não tenha, será criada uma Conta Corrente com o saldo de R$ 1050.
@@ -25,98 +23,102 @@ const ajuda = () =>
         Usuario.ContaXXX.deposito(valor) -> Deposita o "valor" na Conta informada.
         -----------------
         Usuario.ContaXXX.saque(valor) -> Saca o "valor" da Conta informada.
-    \n\n`)
+        \n\n`
+)
 
 console.log(Apresentação)
 
 const listaUsuarios = [
     // UsuarioMaster = {id: 999, cpf: 99999999999, nome: 'Master',
-    // ContaCorrente: {id: 1999, saldo: 99999999}, 
-    // ContaPoupanca:{id: 2999, saldo: 99999999}}
+    // contaCorrente: {id: 1999, saldo: 99999999}, 
+    // contaPoupanca:{id: 2999, saldo: 99999999}}
 ]
 
-let contUsuario = 0
-let contCorrente = 1000
-let contPoupanca = 2000
 
 class Usuario {
     constructor(cpf, nome) {
-        this.id = contUsuario
+        this.id = Usuario.contadorUsuario
         this.cpf = cpf
         this.nome = nome
-        this.ContaCorrente = null
-        this.ContaPoupanca = null
+        this.contaCorrente = null
+        this.contaPoupanca = null
+
+        Usuario.contadorUsuario++
     }
 
+    static contadorUsuario = 0
+
     criarContaPoupanca() {
-        if(this.ContaPoupanca == null) {
-            this.ContaPoupanca = new ContaPoupanca(contPoupanca + this.id)
+        if(this.contaPoupanca == null) {
+            this.contaPoupanca = new ContaPoupanca(1000 + this.id)
+            return true
         } else {
-            console.log(`Usuário já tem Conta Poupança. ID da conta: ${this.ContaPoupanca.id}`)
+            return false
         }
     }
 
     criarContaCorrente() {
-        if(this.ContaCorrente == null) {
-            this.ContaCorrente = new ContaCorrente(contCorrente + this.id)
+        if(this.contaCorrente == null) {
+            this.contaCorrente = new ContaCorrente(2000 + this.id)
+            return true
         } else {
-            console.log(`Usuário já tem Conta Corrente. ID da conta: ${this.ContaCorrente.id}`)
+            return false
         }
     }
 }
 
-class _Conta {
+class Conta {
     constructor(id) {
         this.id = id
     }
 
     deposito(valor) {
         this.saldo += valor
-        console.log(`Depósito de R$ ${valor} efetuado com sucesso.`)
+        return true
     }
 
     saque(valor) {
         if(valor <= this.saldo) {
             this.saldo -= valor
-            console.log(`Saque de R$ ${valor} efetuado com sucesso.`)
+            return true
         } else {
-            console.log(`Saldo insuficiente para saque. Saldo total: R$ ${this.saldo}`)
+            return false
         }
     }
 }
 
-class ContaCorrente extends _Conta {
+class ContaCorrente extends Conta {
     constructor(id) {
         super(id)
         this.saldo = 1050
     }
 
     transferencia(valor, destinatarioConta) {
-        let filtroCorrente = listaUsuarios.filter(conta => (conta.ContaCorrente != null) && destinatarioConta == conta.ContaCorrente.id)
-        let filtroPoupanca = listaUsuarios.filter(conta => (conta.ContaPoupanca != null) && destinatarioConta == conta.ContaPoupanca.id)
+        let filtroCorrente = listaUsuarios.filter(conta => (conta.contaCorrente != null) && destinatarioConta == conta.contaCorrente.id)
+        let filtroPoupanca = listaUsuarios.filter(conta => (conta.contaPoupanca != null) && destinatarioConta == conta.contaPoupanca.id)
 
         if(filtroCorrente.length > 0 || filtroPoupanca.length > 0) {
 
             if(valor <= this.saldo) {
                 if(filtroCorrente.length > 0) {
-                    filtroCorrente[0].ContaCorrente.saldo += valor
+                    filtroCorrente[0].contaCorrente.deposito(valor)
 
                 } else if(filtroPoupanca.length > 0) {
-                    filtroPoupanca[0].ContaPoupanca.saldo += valor
+                    filtroPoupanca[0].contaPoupanca.deposito(valor)
                 }
-                this.saldo -= valor
-                console.log(`Transferencia efetuada com sucesso. Saldo restante em conta: R$ ${this.saldo}`)
+                this.saque(valor)
+                return true
 
             } else if(valor >= this.saldo) {
-                console.log(`Saldo insuficiente para realizar transferência. Saldo em conta: R$ ${this.saldo}`)
+                return false
             }
         } else {
-            console.log('Conta do destinatário não encontrada, gentileza conferir as informações.')
+            return 'Conta do destinatário não encontrada, gentileza conferir as informações.'
         }
     }
 }
 
-class ContaPoupanca extends _Conta {
+class ContaPoupanca extends Conta {
     constructor(id) {
         super(id)
         this.saldo = 480
@@ -126,31 +128,30 @@ class ContaPoupanca extends _Conta {
 function criarUsuario(cpfInformado, nome) {
     let filtro = listaUsuarios.filter(usuario => cpfInformado == usuario.cpf)
 
-    if(filtro.length > 0) {
-        return console.log(`Este usuario já está cadastrado. ID: ${filtro[0].id} \n`)
-    } else {
+    if(filtro.length == 0) {
         listaUsuarios.push(
             new Usuario(cpfInformado, nome)
         )
-        let novoUser = listaUsuarios.filter(usuario => cpfInformado == usuario.cpf)
-        console.log(`Novo Usuário. ID: ${novoUser[0].id}, nome: ${novoUser[0].nome} \n`)
-        contUsuario++
+        return true
+    } else if(cpfInformado == undefined || nome == undefined) {
+        throw new Error('Gentileza chamar a função informando dentro dos parênteses seu CPF e NOME.')
+    } else {
+        throw new Error('Usuário já tem conta no sistema.')
+    }
+}
+function buscaUsuario(cpfOuID) {
+    let filtro = listaUsuarios.filter(usuario => cpfOuID == usuario.id || cpfOuID == usuario.cpf)
+
+    if(filtro.length == 1) {
+        return filtro[0]
+    } else {
+        throw new Error('Usuário não encontado.');
     }
 }
 
-function buscaUsuario(busca) {
-    if(typeof busca == "string") {
-        for(users in listaUsuarios) {
-            if(busca == listaUsuarios[users].nome) {
-                console.log(`Usuário encontrado: ID: ${listaUsuarios[users].id}, Nome: ${listaUsuarios[users].nome}, CPF: ${listaUsuarios[users].cpf};`)
-            }
-        }
-    } else {
-        let filtro = listaUsuarios.filter(usuario => busca == usuario.id || busca == usuario.cpf)
-        if(filtro.length > 0) {
-            console.log(`Usuário encontrado. ID: ${filtro[0].id}, Nome: ${filtro[0].nome}, CPF: ${filtro[0].cpf} \n`)
-        } else {
-            console.log('Usuário não encontrado. Poderá criá-lo utilizando a função criarUsuario() ou checar o CPF/ID informado. \n')
-        }
-    }
-}
+criarUsuario(111, 'patrick')
+console.log(listaUsuarios)
+criarUsuario(222, 'abcdefghijk')
+console.log(listaUsuarios)
+criarUsuario()
+buscaUsuario(111)
